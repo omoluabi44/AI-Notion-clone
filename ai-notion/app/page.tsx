a753f2a1f66e86d868d8c195e32365707d25ec86
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-11-30 17:10:12 +0100

    setup next js, shadcn and clerk

---
+++ ai-notion/app/page.tsx
@@ -0,0 +1,20 @@
+
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+  
+  
+      <main className=" flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <p>hello word </p>
+        <h1>let build ai notion </h1>
+        <h2>let go !!!</h2>
+        <Button> click me </Button>
+      </main>
+       
+     
+         
+  
+  );
+}
